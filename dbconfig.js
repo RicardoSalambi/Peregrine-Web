4621f1928bd7611c0ee46d763c5490000f65db37
RicardoSalambi <ricardox200@gmail.com>
2020-06-21 17:00:08 +0200

    Added a Procfile for Heroku, Change the database details to the one created online 'Cleardb' and enter the engines running for it's enviroment

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
-const host      = 'localhost';
-const user      = 'root';
-const password  = 'admin';
-const database  = 'crudtest';
+const host      = 'us-cdbr-iron-east-04.cleardb.net';
+const user      = 'b4b14493f5eaa1';
+const password  = '7c6777a9';
+const database  = 'heroku_20d6e7bc9dbb3b1';
 
 module.exports = {
     host,
